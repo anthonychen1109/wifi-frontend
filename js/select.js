84a335cededcb05a5e4c566224c8037870dbe8aa
ac065455 <chen.anthony.1109@gmail.com>
2018-01-17 12:50:39 -0500

    added update and delete functions

--- js/select.js
+++ js/select.js
@@ -1,7 +1,7 @@
 
 const url = 'http://wifi.abcpos.com/helper/ajax/portalmanager';
 
-// fetch all function
+// fetch all data on load
 function fetchAll() {
   $.post(url,{get: 'portals' }, function(data) {
     // console.log(data);
@@ -11,12 +11,11 @@ function fetchAll() {
   },"json");
 }
 
-// fetch all data on page load
 $(function() {
   fetchAll();
 });
 
-// fetch data from selected id
+// fetch portal_id
 function fetchId(id) {
   $.post(url,{get: 'portal_info', portal_id: id}, function(data) {
     console.log(data);
@@ -30,18 +29,21 @@ function fetchId(id) {
 
 $('#selector').change(function(){
   // console.log(this.value);
-  if($('#selector').val() > -1)
+  if($('#selector').val() > -1) {
     fetchId(this.value);
+    $('#delete-btn').addClass('show');
+  }
   else {
     $('#dir-name').val('');
     $('#title-name').val('');
     $('#bg-name').val('');
     $('#logo-name').val('');
     $('#landing-name').val('');
+    $('#delete-btn').removeClass('show');
   }
 })
 
-// post data to sql
+// create new portal
 function postId(name, title, background, logo, landingpage) {
   $.post(url,
     {
@@ -50,19 +52,80 @@ function postId(name, title, background, logo, landingpage) {
       background: background,
       logo: logo,
       landingpage: landingpage,
-      portal_id: 5,
+      // portal_id: 6,
       title: title
     },
-    function(name,background,logo,landingpage,portal_id,title) {
-      console.log(name);
+    function(data) {
+      console.log(data);
     },"json");
 }
 
 $('#save-btn').click(function() {
+  let portal_id = $('#selector').val();
   let name = $('#dir-name').val();
   let title = $('#title-name').val();
   let background = $('#bg-name').val();
   let logo = $('#logo-name').val();
   let landingpage = $('#landing-name').val();
-  postId(name, title, background, logo, landingpage);
+  if(portal_id == -1){
+    postId(name, title, background, logo, landingpage);
+    location.reload();
+  } else if (portal_id > 0 && portal_id != '') {
+      updateId(name, title, background, logo, landingpage, portal_id);
+      location.reload();
+    } else {
+      console.log('do nothing');
+    }
 });
+
+// delete portal id
+function deleteId(portal_id) {
+  $.post(url,
+    {
+      set: 'delete',
+      portal_id: portal_id
+    },function(data) {
+      console.log(data);
+    })
+}
+
+$('#delete-btn').click(function() {
+  let portal_id = $('#selector').val();
+  if (portal_id > 0) {
+    deleteId(portal_id);
+    location.reload();
+  }
+  else {
+    console.log('No portal_id selected');
+  }
+});
+
+//update portal_id
+function updateId(name, title, background, logo, landingpage, portal_id){
+  $.post(url, {
+    set: 'portal',
+    name: name,
+    title: title,
+    background: background,
+    landinpage: landingpage,
+    logo: logo,
+    portal_id: portal_id
+  }, function(data) {
+    console.log(data);
+  }, "json");
+}
+
+// $('#update-btn').click(function() {
+//   let portal_id = $('#selector').val();
+//   let name = $('#dir-name').val();
+//   let title = $('#title-name').val();
+//   let background = $('#bg-name').val();
+//   let logo = $('#logo-name').val();
+//   let landingpage = $('#landing-name').val();
+//   if (portal_id > 0 && portal_id != '') {
+//     updateId(name, title, background, logo, landingpage, portal_id);
+//     location.reload();
+//   } else {
+//     console.log('do nothing');
+//   }
+// });
